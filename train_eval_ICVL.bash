fa0a57171303ac07e7d0a9afba94a4e8e835f1ca
Mehal <78313436+MEHALSAKTHI@users.noreply.github.com>
2023-11-12 23:23:10 +0530

    Update train_eval_ICVL.bash

--- train_eval_ICVL.bash
+++ train_eval_ICVL.bash
@@ -1,7 +1,7 @@
 folder="icvl_experiment"
 
 ex1="python main.py --check $folder --config_file configs/icvl.yaml"
-ex2="python eval.py --path $folder/checkpoints"
+ex2="python eval.py --path /kaggle/input/trihorn-saved-models-3d-points-gen"
 
-$ex1
+# $ex1
 $ex2
